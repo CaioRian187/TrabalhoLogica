4fd3ad5ab43ba9926ee8683a3697b9c3ee889267
CaioRian187 <caiorian246810@gmail.com>
2025-07-30 20:22:29 -0300

    Update TentativasTrabalho.py

--- TentativasTrabalho.py
+++ TentativasTrabalho.py
@@ -47,7 +47,7 @@ def gerar_estado_inicial(passos=20):
 
     movimentos = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
 
-    for _ in range(passos):
+    for i in range(passos):
         mov_validos = []
         for acao, (dl, dc) in movimentos.items():
             nl, nc = vazio_linha + dl, vazio_coluna + dc
